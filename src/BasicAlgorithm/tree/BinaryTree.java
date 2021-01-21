88cc3ea0bd2bc360384e1ac18e10ff9770bfd11c
zhuzhihong <846211162@qq.com>
2021-01-21 21:30:36 +0800

    创建二叉树

---
+++ src/BasicAlgorithm/tree/BinaryTree.java
@@ -0,0 +1,27 @@
+package BasicAlgorithm.tree;
+
+/**
+ * @program: algorithm
+ * @description: 创建满二叉树或者完全二叉树
+ * @author: zzh
+ * @create: 2021-01-21 21:07
+ **/
+
+public class BinaryTree<T> {
+    private TreeNode root = null;
+    public BinaryTree(T arr[],int index) {
+        root = createTree(arr,index);
+
+    }
+
+    public TreeNode createTree(int arr[], int index){
+        TreeNode<Integer> treeNode = null;
+        if (index < arr.length){
+            treeNode = new TreeNode<>(arr[index]);
+            treeNode.setLeft(createTree(arr, index*2+1));
+            treeNode.setRight(createTree(arr,index*2+2));
+        }
+        return  treeNode;
+    }
+
+}
---
+++ src/BasicAlgorithm/tree/TreeNode.java
@@ -0,0 +1,41 @@
+package BasicAlgorithm.tree;
+
+/**
+ * @program: algorithm
+ * @description: 树节点
+ * @author: zzh
+ * @create: 2021-01-21 21:09
+ **/
+public class TreeNode<T> {
+    private T data;
+    private TreeNode left;
+    private TreeNode right;
+
+    public TreeNode(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+}
