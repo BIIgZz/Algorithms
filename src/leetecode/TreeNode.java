039fd15a744d41cebf3f89d94c13a71a96135408
zhuzhihong <846211162@qq.com>
2021-01-31 21:44:53 +0800

    树节点

--- src/leetecode/TreeNode.java
+++ src/leetecode/TreeNode.java
@@ -3,14 +3,16 @@ package leetecode;
 /**
  * @program: algorithm
  * @description: 树节点
- * @author: zzh
+ * @author: azzh
  * @create: 2021-01-31 16:19
  **/
 public class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-    TreeNode(int x) { val = x; }
+    TreeNode(int x) {
+        val = x;
+    }
 
 
 }
