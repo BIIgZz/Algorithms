805c3eff99049dc31d553413a02611cff2546772
zzh <846211162@qq.com>
2021-03-11 22:54:58 +0800

    3. 无重复字符的最长子串

---
+++ src/leetecode/LengthOfLongestSubstring.java
@@ -0,0 +1,34 @@
+package leetecode;
+
+import java.util.HashMap;
+
+/**
+ *
+ *
+ *@description: leetecode 3. 无重复字符的最长子串
+ *@author: zzh
+ *@time: 2021/3/10 下午5:15
+ * 
+ */
+public class LengthOfLongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0)
+            return 0;
+        int max = 0;
+        int left = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max,i - left + 1);
+        }
+        return max;
+    }
+    public static void main(String[] s){
+        LengthOfLongestSubstring length = new LengthOfLongestSubstring();
+        int abcabcbb = length.lengthOfLongestSubstring("abcabcbb");
+        System.out.println(abcabcbb);
+    }
+}
