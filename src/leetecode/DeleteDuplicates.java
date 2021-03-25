4d8e4fd86c7894e9a0c1872e948698739d57f2e1
BIIgZz <846211162@qq.com>
2021-03-25 20:59:19 +0800

    82. 删除排序链表中的重复元素 II

--- src/leetecode/DeleteDuplicates.java
+++ src/leetecode/DeleteDuplicates.java
@@ -3,11 +3,10 @@ package leetecode;
 /**
  *
  *
- *@description: leetecode 82. 删除排序链表中的重复元素 II存在一个按升序排列的链表，
+ *@description: leetecode 82. 删除排序链表中的重复元素 II
+ * 存在一个按升序排列的链表，
  * 给你这个链表的头节点 head ，请你删除链表中所有存在数字重复情况的节点，只保留原始链表中 没有重复出现 的数字。
- *
  * 返回同样按升序排列的结果链表。
-
  *
  *@author: zzh
  *@time: 2021/3/25 下午4:06
