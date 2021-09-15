f49849749ae5e4ef7dfc70bcabeaaae21cc4359c
zzh <846211162@QQ.COM>
2021-09-15 11:21:37 +0800

    876. 链表的中间结点

---
+++ src/leetecode/MiddleNode.java
@@ -0,0 +1,12 @@
+package leetecode;
+
+public class MiddleNode {
+    public ListNode middleNode(ListNode head) {
+        ListNode p = head, q = head;
+        while (q != null && q.next != null) {
+            q = q.next.next;
+            p = p.next;
+        }
+        return p;
+    }
+}
